ccd7d948bea257e463b115c6a4f5c568f8831375
nguyendoanhau <nguendoanhau@gmail.com>
2024-01-18 08:58:05 +0700

    Add screens file

---
+++ src/components/Button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Pressable, Text, TouchableOpacity} from 'react-native';
+import {styles} from './styles';
+const Button = ({title}) => {
+    const handlePress = () =>{
+        console.log('Clicked');
+    };
+    return (
+        <TouchableOpacity
+        activeOpacity= "0.6."
+        onPress={handlePress}
+        style={styles.container}>
+            <Text style={styles.textContent}>{title}</Text>
+        </TouchableOpacity>
+    );
+};
+export default Button;
---
+++ src/screens/Auth/Splash/styles.js
@@ -0,0 +1,29 @@
+import  {StyleSheet} from 'react-native';
+export const styles=StyleSheet.create({
+    container:{
+        pading:24,
+    },
+    img: {
+        with:'100%',
+        height: 200,
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign:'center',
+    },
+    innerTitle: {
+        collor:'#FCA34D',
+        textDecorationLine: 'underline',
+    },
+    titleContainer: {
+        marginVertical: 54,
+    },
+    footerText: {
+        color: '#4F63AC',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 30,
+    },
+});
