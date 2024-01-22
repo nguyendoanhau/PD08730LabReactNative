9ab6ebfd1ad59d282245fb47de2637c6b2989adf
nguyendoanhau <nguendoanhau@gmail.com>
2024-01-22 23:10:09 +0700

    PD08730

---
+++ src/components/ListView/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import{Text, View, FlatList, Image} from 'react-native';
+import {styles} from './styles';
+const DATA=[
+    {
+    id:'1',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'2',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'3',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'4',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'5',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'6',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'7',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+{
+    id:'8',
+    title:'Data Structures',
+    imageSource:require('../../assets/image/ronando.png')
+},
+];
+const Item=({title,imageSource})=> {
+    return (
+        <View style={styles.item}>
+            <Image source={imageSource} style={styles.avatar}></Image>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+        );
+};
+const renderItem=({item})=>(
+    <Item imageSource={item.imageSource} title={item.title}/>
+);
+
+const ListCourse = () => {
+    return(
+        <View style={styles.container}>
+            <FlatList  data={DATA} 
+            renderItem={renderItem}
+            keyExtractor={item => item.id}/>
+        </View>
+    );
+};
+export default React.memo(ListCourse);
--- src/screens/Lab4/index.js
+++ src/screens/Lab4/index.js
@@ -1,62 +1,12 @@
 import React from "react";
-import{Text, View, FlatList, Image} from 'react-native';
-import {styles} from './styles';
-const DATA=[
-    {
-    id:'1',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'2',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'3',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'4',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'5',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'6',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'7',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-{
-    id:'8',
-    title:'Data Structures',
-    imageSource:require('../../assets/image/ronando.png')
-},
-];
-const Item=({title,imageSource})=> {
-    return (
-        <View style={styles.item}>
-            <Image source={imageSource} style={styles.avatar}></Image>
-            <Text style={styles.text}>{title}</Text>
-        </View>
-        );
-};
-const renderItem=({item})=>(
-    <Item imageSource={item.imageSource} title={item.title}/>
-);
-
-const ListCourse = () => {
+import { Text, View } from "react-native";
+import ListCourse from '../../components/ListView';
+import { ScrollView } from "react-native-gesture-handler";
+const Lab4 = () =>{
     return(
-        <View style={styles.container}></View>
-    )
-}
+        <View>
+            <ListCourse/>
+        </View>
+    );
+};
+export default React.memo(Lab4);
